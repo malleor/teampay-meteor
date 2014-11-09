45e8c73df227d6fc5b420e33aea56bb6c5cca891
Janusz Lenar <malleor@gmail.com>
2014-11-09 12:52:20 +0100

    Multiple templates switching PoC
    Plus, refactored the project structure

---
+++ client/index.js
@@ -0,0 +1,42 @@
+var headline = "Headline",
+    content_template = "ABC",
+    button = "QQQ",
+    page_state_dep = new Tracker.Dependency();
+
+
+
+Template.page.helpers({
+  headline: function(){
+    page_state_dep.depend();
+    return headline;
+  },
+  content: function(){
+    page_state_dep.depend();
+    return Template[content_template];
+  },
+  button: function(){
+    page_state_dep.depend();
+    return button;
+  }
+});
+
+Template.page.events({
+  'click #QQQ': function(){
+    console.log("switching to QQQ");
+    content_template = "QQQ";
+    button = "FUU";
+    page_state_dep.changed();
+  },
+  'click #FUU': function(){
+    headline = "Headline";
+    content_template = "FUU";
+    button = "ABC";
+    page_state_dep.changed();
+  },
+  'click #ABC': function(){
+    headline = "Headline";
+    content_template = "ABC";
+    button = "QQQ";
+    page_state_dep.changed();
+  }
+});
